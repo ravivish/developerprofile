b0f3fbeca7e707b0ac986103b3aeadb712ab5aca
Ravi Vishwakarma <ravivishwakarma825@gmail.com>
2021-05-15 21:42:41 +0530

    fixed nav links

--- src/DeveloperRepos/DeveloperRepos.js
+++ src/DeveloperRepos/DeveloperRepos.js
@@ -11,34 +11,35 @@ class DeveloperRepos extends Component {
     console.log(repo);
     return (
       <div className="repo-list">
+        <h3>Github Repositories</h3>
         <hr />
         <ul className="repo-list-item">
-        <h3>Github Repositories</h3>
-          { repo && repo.map((e, index) => {
-            return (
-              <li key={index}>
-                <a rel="noreferrer" target="_blank" href={e.html_url}>
-                  {e.name}
-                  <span>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      enableBackground="new 0 0 24 24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      width="24"
-                      className="redirect-arrow"
-                    >
-                      <rect fill="none" height="24" width="24"></rect>
-                      <path d="M9,5v2h6.59L4,18.59L5.41,20L17,8.41V15h2V5H9z"></path>
-                    </svg>
-                  </span>
-                </a>
-                {e.updated_at}
-                {e.description}
-              </li>
-            );
-          })}
-          {/* <hr className="bottom-line" /> */}
+          {repo &&
+            repo.map((e, index) => {
+              return (
+                <li key={index}>
+                  <a rel="noreferrer" target="_blank" href={e.html_url}>
+                    {e.name}
+                    <span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        enableBackground="new 0 0 24 24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        width="24"
+                        className="redirect-arrow"
+                      >
+                        <rect fill="none" height="24" width="24"></rect>
+                        <path d="M9,5v2h6.59L4,18.59L5.41,20L17,8.41V15h2V5H9z"></path>
+                      </svg>
+                    </span>
+                  </a>
+                  {e.updated_at}
+                  {e.description}
+                  <hr className="bottom-line" />
+                </li>
+              );
+            })}
         </ul>
       </div>
     );
